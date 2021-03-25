b4b528d2e1b15e7a62a8445a1aa7a0898c6cbe50
Ryan Miller <rmiller07@gmail.com>
2021-03-25 11:21:32 -0400

    K error resolved in tests, but lower amounts fail with insufficient

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -19,7 +19,7 @@ def test_operation(accounts, token, vault, strategy, strategist, amount, user, c
     pairs = [strategy.ethCrvPair(), strategy.ethYveCrvPair(), strategy.ethUsdcPair()]
     for pair in pairs:
         Contract.from_explorer(pair, owner=strategist).sync()
-        
+
     # harvest
     crv3.transfer(strategy, 10e20, {"from": whale_3crv})
     strategy.harvest()
@@ -40,44 +40,6 @@ def test_emergency_exit(accounts, token, vault, strategy, strategist, amount, us
     strategy.setEmergencyExit()
     strategy.harvest()
     assert token.balanceOf(strategy.address) < amount
-
-
-def test_profitable_harvest(
-    gov,
-    vault,
-    strategy,
-    token,
-    amount,
-    weth_amount,
-    yveCrv,
-    weth,
-    usdc,
-    crv3,
-    chain,
-    whale_3crv,
-    user
-):
-    # Deposit to the vault and harvest
-    # print(yveCrv.strategies(strategy)) # Strategy params (perf fee, activation, debtraatio, mindebtperharvest, maxdebtperharvest, lastreport, totaldebt)
-    chain.snapshot()
-    token.approve(vault.address, amount, {"from": user})
-    vault.deposit(amount, {"from": user})
-    assert token.balanceOf(vault.address) == amount
-
-    showBalances(token, vault, strategy, yveCrv, weth, usdc, crv3)
-
-    strategy.harvest()
-    assert token.balanceOf(strategy.address) == amount
-
-    # showBalances(token, vault, strategy, yveCrv, weth, usdc, crv3)
-
-    # Simulate a claim by sending some 3Crv to the strategy before harvest
-    crv3.transfer(strategy, 10e20, {"from": whale_3crv})
-    strategy.harvest()
-    print("\n\n~~After Harvest #2~~")
-    showBalances(token, vault, strategy, yveCrv, weth, usdc, crv3)
-    assert token.balanceOf(strategy.address) > amount
-    chain.revert()
 
 def test_set_buffer(gov, token, vault, strategy, strategist):
     # Deposit to the vault and harvest
